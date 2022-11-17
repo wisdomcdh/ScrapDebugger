29e9a027d63e6b3e92e65236774145bc40d5cce1
woody.ho <wisdomcdh@gmail.com>
2022-11-17 05:36:34 +0000

    fix bug

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOMClient.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
